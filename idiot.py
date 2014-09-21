2dc770c8c41eb43fc7383c023b788648ac02346e
zhizouxiao <zhizouxiao@gmail.com>
2014-09-21 13:56:58 +0800

    idiot2.0

--- idiot.py
+++ idiot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
+    Idiot
     ~~~~~~
 
     A microblog example application written as Flask tutorial with
@@ -14,21 +14,42 @@ import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from flask_bootstrap import Bootstrap
 
 # create our little application :)
 app = Flask(__name__)
-
+Bootstrap(app)
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'idiot.db'),
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+from flask.ext.wtf import Form
+from wtforms import StringField, DateField, FloatField, SubmitField, TextAreaField
+from wtforms.validators import Required
+import datetime
 
+class LoginForm(Form):
+    username = StringField('username', validators=[Required()])
+    password= StringField('password', validators=[Required()])
+    submit = SubmitField('Login')
 
+class PayoutForm(Form):
+    username = StringField('username', validators=[Required()])
+    apply_money = FloatField('apply_money', validators=[Required()])
+    actual_money = FloatField('acutal_money', validators=[Required()])
+    note = TextAreaField('note', validators=[Required()])
+    submit = SubmitField('Login')
+
+class IncomeForm(Form):
+    username = StringField('username', validators=[Required()])
+    money = FloatField('money', validators=[Required()])
+    note = TextAreaField('note', validators=[Required()])
+    submit = SubmitField('Login')
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -44,7 +65,7 @@ def init_db():
     db.commit()
 
 
-def initdb_command():
+def initdb_commaactual_moneynd():
     """Creates the database tables."""
     init_db()
     print('Initialized the database.')
@@ -67,28 +88,40 @@ def close_db(error):
 
 
 @app.route('/')
-def show_entries():
+def index():
     db = get_db()
     cur = db.execute('select * from account order by datetime desc')
     entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    return render_template('index.html', entries=entries)
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
+@app.route('/payout', methods=['GET', 'POST'])
+def payout():
     if not session.get('logged_in'):
         abort(401)
 
-    db = get_db()
-    print "+"*30, db, request.form['apply_money'], request.form['username']
-    db.execute('insert into account(username, apply_money, note) values (?, ?, ?)',
-               [request.form['username'], request.form['apply_money'], request.form['note']])
-    print "+"*30, '____'
-    db.commit()
-    print "+"*30
-    flash('New entry was successfully posted')
-    print "+"*30
-    return redirect(url_for('show_entries'))
+    if request.method == 'POST':
+        db = get_db()
+        db.execute('insert into account(username, apply_money, actual_money, note, datetime) values (?, ?, ?, ?, ?)',
+               [request.form['username'], request.form['apply_money'], request.form['actual_money'], request.form['note'], datetime.datetime.now()])
+        db.commit()
+        flash('Pay out success! ')
+    form = PayoutForm()
+    return render_template('payout.html', form=form)
+
+@app.route('/income', methods=['GET', 'POST'])
+def income():
+    if not session.get('logged_in'):
+        abort(401)
+
+    if request.method == 'POST':
+        db = get_db()
+        db.execute('insert into account(username, apply_money, actual_money, note, datetime) values (?, ?, ?, ?, ?)',
+               [request.form['username'], request.form['money'], request.form['money'], request.form['note'], datetime.datetime.now()])
+        db.commit()
+        flash('Income success!')
+    form = IncomeForm()
+    return render_template('income.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -102,15 +135,16 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+            return redirect(url_for('index'))
+    form = LoginForm()
+    return render_template('login.html', error=error, form=form)
 
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
